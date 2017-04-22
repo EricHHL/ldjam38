1d7a2d0b4988207c04c626b7e9e309377e2aad21
EricHHL <e.h.h.lopes@hotmail.com>
2017-04-22 10:58:32 -0400

    Carregando tileset

--- mapRenderer.lua
+++ mapRenderer.lua
@@ -1,11 +1,38 @@
 
 MapRenderer = Script({GameHexMap})
+local map
+
+local function loadTileset(name)
+	map = require("textures."..name)
+	map.tiles = {}
+
+	local ts = map.tilesets[1]
+	map.texture = R.get("tileset", ts.image)
+	local curID = ts.firstgid
+	ts.lastid = curID + ts.tilecount
+	for j=0, (ts.imageheight/ts.tileheight)-1 do
+		for i=0, (ts.imagewidth/ts.tilewidth)-1 do
+			map.tiles[curID] = love.graphics.newQuad(i*ts.tilewidth, j*ts.tileheight, ts.tilewidth, ts.tileheight, ts.imagewidth, ts.imageheight)
+			curID = curID + 1
+		end
+	end
+
+	for k,t in pairs(ts.tiles) do
+		for k,p in pairs(t.properties) do
+			map.tiles[t.id+1][k] = p
+		end
+	end
+
+end
 
 function MapRenderer:init(m)
 	print("criou MapRenderer")
+	loadTileset("tileset")
 end
 
 function MapRenderer:draw(m)
-	print("draw")
 	
+	love.graphics.draw(map.texture, map.tiles[2], 100, 100)
 end
+
+
---
+++ textures/tileset.lua
@@ -0,0 +1,32 @@
+return {
+  version = "1.1",
+  luaversion = "5.1",
+  tiledversion = "0.17.1",
+  orientation = "orthogonal",
+  renderorder = "left-up",
+  tilewidth = 122,
+  tileheight = 140,
+  nextobjectid = 1,
+  properties = {},
+  tilesets = {
+    {
+      name = "tileset",
+      firstgid = 1,
+      tilewidth = 120,
+      tileheight = 140,
+      spacing = 2,
+      margin = 0,
+      image = "../textures/tileset.png",
+      imagewidth = 1024,
+      imageheight = 2048,
+      tileoffset = {
+        x = 0,
+        y = 0
+      },
+      properties = {},
+      terrains = {},
+      tilecount = 112,
+      tiles = {}
+    }
+  }
+}
