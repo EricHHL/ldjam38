677d9e3362fa1fa6c85e242a6707539bf904055e
higormenezes <higor_menezes96@hotmail.com.br>
2017-04-23 19:31:47 -0400

    menu
    menu

--- guis/gameSceneGuiMenu.lua
+++ guis/gameSceneGuiMenu.lua
@@ -67,20 +67,5 @@ function gui:draw()
 end
 
 
-function love.mousereleased(x, y, button, isTouch)
-    gooi.released()
-end
-
-function love.mousepressed(x, y, button, istouch)
-    gooi.pressed()
-end
-
--- function love.keypressed(key, scancode, isrepeat)
---     gooi.keypressed()
--- end
-
-function love.textinput(text)
-    gooi.textinput(text)
-end
 
 return gui
