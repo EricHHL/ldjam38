9553c31c988d98966dbf070ba91146aab2f5d760
Luiz <luizcarlos1405@gmail.com>
2017-04-23 21:03:14 -0400

    corrigido probleminha na animação de renovação

--- animaCards.lua
+++ animaCards.lua
@@ -10,7 +10,9 @@ function CardsAnimator:init(t)
 end
 
 function CardsAnimator:drawAfter(t)
+    camera:detach()
     love.graphics.setColor(255, 255, 255, t.pos.fade)
 	love.graphics.draw(MapRenderer.texture, MapRenderer.quads[t.animaCards.tile], t.pos.x, t.pos.y, 0, t.animaCards.scale.sx, t.animaCards.scale.sy, 0, 0)
     love.graphics.setColor(255, 255, 255, 255)
+    camera:attach()
 end
